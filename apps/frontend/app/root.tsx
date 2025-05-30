e621c46c84d9e4c9e529a4ccd6038d2d9ce7a092
Raphael Stäbler <raphael.staebler@productionbuild.de>
2025-05-30 10:48:56 +0200

    chore: host fonts locally

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -11,19 +11,9 @@ import { ClientStyleContext } from './utils/client-style-context';
 import { useAppTheme } from './utils/theme';
 
 export const links: LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
   {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
+    href: '/fonts/local-fonts.css',
   },
 ];
 
